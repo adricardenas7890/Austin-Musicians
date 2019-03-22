e2d39b2859b46a3b6d3cb471eb93295c1c51db66
Adriana Cardenas <adri.cardenas@utexas.edu>
2019-03-22 09:16:52 -0700

    main.py updated with url, must fill in the rest for shows

--- website/main.py
+++ website/main.py
@@ -24,11 +24,48 @@ def venues():
 
 @app.route('/shows/')
 def shows():
+
 	return render_template('shows.html')
 
 '''
-	add in functions for artists, venues, shows!!!!!!!!!!!
+	render artists
 '''
+
+@app.route('/artists/explosions_in_the_sky/')
+def explosions_in_the_sky():
+	return render_template('artists/explosions_in_the_sky.html')
+
+@app.route('/artists/octopus_project/')
+def octopus_project():
+	return render_template('artists/octopus_project.html')
+
+@app.route('/artists/tc_superstar/')
+def tc_superstar():
+	return render_template('artists/tc_superstar.html')
+
+'''
+	render venues
+'''
+
+@app.route('/venues/cheer_up_charlies')
+def cheer_up_charlies():
+	return render_template('venues/cheer_up_charlies.html')
+
+@app.route('/venues/empire_control_room')
+def empire_control_room():
+	return render_template('venues/empire_control_room.html')
+
+@app.route('/venues/mohawk')
+def mohawk():
+	return render_template('venues/mohawk.html')
+
+
+'''
+	render shows: FILL IN
+'''
+
+
+
 
 
 if __name__ == "__main__":
