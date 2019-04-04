b9842a8447a8ad7737942928aeea15f9d680c72a
ChristianGil <cmg580@utexas.edu>
2019-04-03 21:33:28 -0500

    Finished adding artists

--- website/create_db.py
+++ website/create_db.py
@@ -48,43 +48,6 @@ def create_bands():
 
     # For each band
     for i, band in enumerate(bands):
-
-        if band['What year did they start making music'] == "None":
-            band['What year did they start making music'] = 0000
-
-        newBand = Artist(id = i,
-                        band_name = band['Artist name'],
-                        members = band['Group name, if none then put none'],
-                        genre = band['Genre of music'],
-                        is_shows = False,
-                        year_started = band['What year did they start making music'])
-        
-        db.session.add(newBand)
-        db.session.commit()
-
-def create_venues():
-    venues = load_json('venues.json')
-
-    for i, venue in enumerate(bands):
-        if venue['Venue'] == "":
-            band['Venue'] = "!!Missing Venue Name!!"
-
-        if venue['Website'][:8] != "https://":
-            venue['Website'] = 
-
-        newVenue = Venue(id = i,
-                        venue_name = band['Venue'],
-                        location = band['Location (do not include Austin,TX, ZIP)'],
-                        genres = band['Genres (Format: \"Genre, Genre\")'],
-                        days_open = band['Days Open (Format: \"Genre, Genre\")'],
-                        hours_open = band['Hours (Format: \"Mon-Sun\")'],
-                        image_link = band['Images'],
-                        information = band['Information (Keep short, use google/wiki)'],
-                        website_link = band['Website'])
-
-        db.session.add(newVenue)
-        db.session.commit()
-
         # Get the data
         group =             band["group"]
         artists =           band["artists"]
@@ -126,6 +89,29 @@ def create_venues():
 
         # db.session.add(newBand)
         # db.session.commit()
+
+def create_venues():
+    venues = load_json('venues.json')
+
+    for i, venue in enumerate(bands):
+        if venue['Venue'] == "":
+            band['Venue'] = "!!Missing Venue Name!!"
+
+        if venue['Website'][:8] != "https://":
+            venue['Website'] = 
+
+        newVenue = Venue(id = i,
+                        venue_name = band['Venue'],
+                        location = band['Location (do not include Austin,TX, ZIP)'],
+                        genres = band['Genres (Format: \"Genre, Genre\")'],
+                        days_open = band['Days Open (Format: \"Genre, Genre\")'],
+                        hours_open = band['Hours (Format: \"Mon-Sun\")'],
+                        image_link = band['Images'],
+                        information = band['Information (Keep short, use google/wiki)'],
+                        website_link = band['Website'])
+
+        db.session.add(newVenue)
+        db.session.commit()
 		
 create_bands()
 # end of create_db.py
