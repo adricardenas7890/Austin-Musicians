85b230b6beb82d0c9ed9df8181a59cff722ed25b
ChristianGil <cmg580@utexas.edu>
2019-04-03 21:01:00 -0500

    Uploaded Some methods for Create DB

--- website/create_db.py
+++ website/create_db.py
@@ -1,6 +1,6 @@
 # beginning of create_db.py
 import json
-from models import app, db, Artist
+from models import *
 import os
 
 current = os.path.dirname( os.path.realpath(__file__) )
@@ -12,22 +12,78 @@ def load_json(filename):
 
     return jsn
 
+# Function to remove extra commas
+def removeExtraCommas( inputString ):
+    inputString = inputString.split(",")
+    inputString = inputString.clear("")
+    inputString = ",".join(inputString)
+    return inputString
+# Function to capitalize the first letter of each word
+def capitalizeFirst( inputString ):
+    inputString = inputString.lower()
+    inputString = inputString.title()
+    return inputString
+# Function to check that each link starts with http or https
+def isLinkValid( inputLink ):
+    http = inputLink.startswith("http://")
+    https = inputLink.startswith("https://")
+    return http or https
+# Remove all but first link
+def removeAllButFirstLink( inputLink ):
+    inputLink = inputLink.split("\n")
+    inputLink = inputLink.clear("")
+    if len(inputLink) > 1:
+        return inputLink[0]
+    else:
+        return "".join(inputLink)
+
 def create_bands():
-    bands = load_json('music.json')
+    # Load the bands in
+    bands = load_json('database/artists.json')
 
+    # For each band
     for i, band in enumerate(bands):
-        if band['What year did they start making music'] == "None":
-            band['What year did they start making music'] = 0000
+        # Get the data
+        group =             band["group"]
+        artists =           band["artists"]
+        genre =             band["Genre"]
+        year_started =      band["year_started"]
+        group_summary =     band["group_summary"]
+        image =             band["image"]
+        albums =            band["albums"]
+        tour =              band["tour"]
+        external_links =    band["external_links"]
+        social_media =      band["social_media"]
+        email =             band["email"]
 
-        newBand = Artist(id = i,
-                        band_name = band['Artist name'],
-                        members = band['Group name, if none then put none'],
-                        genre = band['Genre of music'],
-                        is_shows = False,
-                        year_started = band['What year did they start making music'])
-        
-        db.session.add(newBand)
-        db.session.commit()
+        # Clean artists - extra commas and capitilization
+        artists = capitalizeFirst( removeExtraCommas( artists ) )
+        # Clean genre - extra commas and capitilization
+        genre = capitalizeFirst( removeExtraCommas( genre ) )
+        # Make sure that year_started is an int
+        if type(year_started) != int:
+            year_started = 0
+        # Check that the image link is valid
+        if not isLinkValid( image ):
+            image = ""
+        image = removeAllButFirstLink( image )
+        # Check that the tour link is valid
+        if not isLinkValid( tour ):
+            tour = ""
+        tour = removeAllButFirstLink( tour )
+        # Check that the external links are valid
+        if not isLinkValid( external_links ):
+            external_links = ""
+        external_links = removeAllButFirstLink( tour )
+        # If email == none or None, set as blank
+        if email.lower() == "none" or email.lower() == "na" or email.lower() == "n/a":
+            email = ""
+
+        # Create our Band Object
+
+
+        # db.session.add(newBand)
+        # db.session.commit()
 		
 create_bands()
 # end of create_db.py
